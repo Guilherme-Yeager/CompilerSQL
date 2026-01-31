04281edfdfa9ceb43f6f2a1d8bc9a3425aed96fb
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2026-01-31 18:54:22 -0300

    feat: Funcionalidades add. Closes #35

--- compiler/src/semantic/semantic_visitor.py
+++ compiler/src/semantic/semantic_visitor.py
@@ -5,9 +5,15 @@ from compiler.src.schema.schema import Schema
 
 class SemanticVisitor(AbstractVisitor):
 
+    TIPO_OBJETO = 'object'
+    TIPO_INT = 'int'
+    TIPO_STR = 'string'
+
     def __init__(self, schema):
         self.printer = Visitor()
         self.schema = schema
+        self.table_atual = None
+        self.comando_atual = ''
         self.n_errors = 0
 
     def visitEmptyScript(self, _):
@@ -44,13 +50,40 @@ class SemanticVisitor(AbstractVisitor):
         st.endScope()
 
     def visitDelete(self, delete):
-        pass
+        st.beginScope('delete')
+        delete.table.accept(self)
+        if not self.schema.existe_tabela(delete.table.name):
+            print(
+                f"\n[Erro] <Comando #{self.printer.aux_printer.pos_command} (Delete)> : Tabela '{delete.table.name}' não existe no schema.")
+            self.n_errors += 1
+        else:
+            if delete.where:
+                self.comando_atual = 'Delete'
+                self.table_atual = delete.table.name
+                delete.where.accept(self)
+        self.comando_atual = ''
+        self.table_atual = None
+        st.endScope()
 
     def visitDropDatabase(self, command):
-        pass
+        st.beginScope('delete')
+        command.database.accept(self)
+        nome_banco = command.database.name
+        if not self.schema.existe_banco(nome_banco):
+            print(
+                f"\n[Erro] <Comando #{self.printer.aux_printer.pos_command} (DROP DATABASE)> : Banco de dados '{nome_banco}' não existe.")
+            self.n_errors += 1
+        st.endScope()
 
     def visitDropTable(self, command):
-        pass
+        st.beginScope('delete')
+        command.table.accept(self)
+        nome_tabela = command.table.name
+        if not self.schema.existe_tabela(nome_tabela):
+            print(
+                f"\n[Erro] <Comando #{self.printer.aux_printer.pos_command} (DROP TABLE)> : Tabela '{nome_tabela}' não existe no schema.")
+            self.n_errors += 1
+        st.endScope()
 
     def visitSelect(self, select):
         pass
@@ -65,19 +98,26 @@ class SemanticVisitor(AbstractVisitor):
         pass
 
     def visitFactorId(self, factor):
-        pass
+        return SemanticVisitor.TIPO_OBJETO
 
     def visitFactorInt(self, number):
-        pass
+        return SemanticVisitor.TIPO_INT
 
     def visitFactorString(self, string):
-        pass
+        return SemanticVisitor.TIPO_STR
 
     def visitFactorGrouping(self, grouping):
         pass
 
     def visitExpressionComparison(self, expression):
-        pass
+        tipo_esq = self.validar_tipo_objeto_coluna(expression.left)
+        tipo_dir = self.validar_tipo_objeto_coluna(expression.right)
+        if tipo_esq is None or tipo_dir is None:
+                return
+        if tipo_esq != tipo_dir:
+            print(f"\n[Erro] <Comando #{self.printer.aux_printer.pos_command} ({self.comando_atual})> : "
+                f"Incompatibilidade de tipos: Comparação entre {tipo_esq} e {tipo_dir}")
+            self.n_errors += 1
 
     def visitExpressionBool(self, expression):
         pass
@@ -93,6 +133,30 @@ class SemanticVisitor(AbstractVisitor):
 
     def visitUpdate(self, update):
         pass
+
+    def buscar_tipo_coluna(self, coluna):
+        '''
+        Busca o tipo real de uma coluna, antes validando
+        se ela existe na tabela.
+
+        Args:
+            coluna: FactorId que é a coluna.
+
+        Returns:
+            str: O tipo da coluna.
+            None: Caso a coluna não exista na tabela atual.
+        '''
+        tipo = coluna.accept(self)
+        if tipo == SemanticVisitor.TIPO_OBJETO:
+            nome_coluna = coluna.name
+            if not self.schema.existe_coluna(self.table_atual, nome_coluna):
+                print(f"\n[Erro] <Comando #{self.printer.aux_printer.pos_command}> ({self.comando_atual}) : "
+                      f"Coluna '{nome_coluna}' não existe na tabela '{self.table_atual}'.")
+                self.n_errors += 1
+                return None
+            return self.schema.buscar_tipo_coluna(self.table_atual, nome_coluna)
+        return tipo
+
 
 
 def main(text_sql=None, schema=None, mode_output=1):
@@ -111,7 +175,7 @@ def main(text_sql=None, schema=None, mode_output=1):
     svisitor.printer.aux_printer.mode = mode_output
     if result is not None:
         result.accept(svisitor)
-        print(f"\nForam encontrados {svisitor.n_errors} erros")
+        print(f"\nForam encontrados {svisitor.n_errors} erros.")
 
 
 if __name__ == "__main__":
